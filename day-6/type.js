7548e3e6b2409ccc708c944c4f83175bdd3c4c29
wp-akash <alaminakash145@gmail.com>
2024-05-04 01:01:45 +0600

    number method | toFixed | toPrecision

---
+++ day-6/type.js
@@ -0,0 +1,29 @@
+/* var name = "Hello World";
+var age  = 25;
+console.log(typeof(age)); */
+
+// Convart Data Type in below
+
+/* var name = "25";
+var age  = 25;
+  age = toString(age);
+convartNumber = parseInt(name);
+console.log(typeof(convartNumber)); */ 
+
+// toString
+// parseInt
+// parseFloat
+// toFixed 
+// toPrecision (Work Number of Lenth)
+// Number (Convart Any type value in number)
+
+
+// toFixed, toPrecision
+/* var num1 = 12.25368;
+ console.log(num1.toFixed(2))
+console.log(num1.toPrecision(2)) */
+
+// Some of Number variable value
+var num1 = "25";
+console.log(typeof(Number(num1)))
+console.log(Number(true))
