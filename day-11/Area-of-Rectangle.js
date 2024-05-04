933ec16f2e0d1e2978da357ae7975713c49cd8cf
wp-akash <alaminakash145@gmail.com>
2024-05-05 01:50:18 +0600

    Area of various shapes

---
+++ day-11/Area-of-Rectangle.js
@@ -0,0 +1,4 @@
+var areaLength = Number(prompt("Enter Area Length = "));
+var areaWidth = Number(prompt("Enter Area Width = "));
+var area = areaLength * areaWidth;
+document.write("Totle Area = "+ area)
