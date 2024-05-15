842bdf1a6ec9aa2fcf0371c451b9d63841cb19e3
wp-akash <alaminakash145@gmail.com>
2024-05-16 01:20:54 +0600

    how to use for loop in javascript (part-1)

---
+++ day-17/task.js
@@ -0,0 +1,22 @@
+var letter = prompt("Enter A Letter = ");
+// console.log(typeof(letter))
+switch (letter) {
+  case "A":
+    document.write("Vowel");
+    break;
+  case "E":
+    document.write("Vowel");
+    break;
+  case "I":
+    document.write("Vowel");
+    break;
+  case "O":
+    document.write("Vowel");
+    break;
+  case "U":
+    document.write("Vowel");
+    break;
+  default:
+    document.write("Consonent");
+    break;
+}
